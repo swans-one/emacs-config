27335e7fb377ff3d05906ea62ce75c02a97ca2c2
Erik Swanson <theerikswanson@gmail.com>
2013-06-10 13:16:35 -0700

    Added a file for keybindings.
    Also added a simple way to switch windows:
    
        C-o       ->   'other window
    
    Also added a simple way to do magit status:
    
        C-x C-g   ->   'magit-status

---
+++ keybindings.el
@@ -0,0 +1,9 @@
+;;
+;; All global changes to keybindings go in this file.
+;;
+
+;; Switch windows with one keystroke
+(global-set-key (kbd "C-o") 'other-window)
+
+;; Magit-status
+(global-set-key (kbd "C-x C-g") 'magit-status)
