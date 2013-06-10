9e9f84091f8770e4012f2a362462ad14097a7750
Erik Swanson <theerikswanson@gmail.com>
2013-06-10 11:01:18 -0700

    Re-arranged init.el, added some documentation.
    It makes sense to have the smaller customizations at the top of the
    file. This way, if something breaks I still have ido mode and backups
    are still saved outside of the current directory.
    
    Also, having them at the top makes them more visible, and more likely
    to be re-factored if anything gets out of control.

--- init.el
+++ init.el
@@ -1,28 +1,17 @@
-;; Set path to dependencies
-(setq site-lisp-dir
-      (expand-file-name "site-lisp" user-emacs-directory))
-
-;; Set up load path
-(add-to-list 'load-path user-emacs-directory)
-(add-to-list 'load-path site-lisp-dir)
-
-;; Add the following subdirectories of the `site-lisp-dir`
-(let ((default-directory site-lisp-dir))
-  (normal-top-level-add-to-load-path
-   '("dcpu16-mode" 
-     "puppet-mode")
-   ))
-
-;; Require other modes
-(require 'dcpu16-mode)
-(require 'puppet-mode)
-
-;; Mode Mappings
-(require 'mode-mappings)
-
+;; This file is broken up into two sections. 
 ;;
-;; Simple Customizations
+;; The first section contains simple customizations, which make sense
+;; to include in this init file. These are customizations that I want
+;; to make sure are loaded, even if something later on fails.
 ;;
+;; The second section contains instructions to load other modules and
+;; bits of code, which make more drastic changes to the
+;; environment. This includes loading other packages and major modes.
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;        Simple Customizations         ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Ido Mode:
 (require 'ido)
@@ -50,4 +39,31 @@
       `((".*" . ,user-temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,user-temporary-file-directory t)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;         Loading Other Elisp          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;; Set path to dependencies
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+
+;; Set up load path
+(add-to-list 'load-path user-emacs-directory)
+(add-to-list 'load-path site-lisp-dir)
+
+;; Add the following subdirectories of the `site-lisp-dir`
+(let ((default-directory site-lisp-dir))
+  (normal-top-level-add-to-load-path
+   '("dcpu16-mode" 
+     "puppet-mode")
+   ))
+
+;; Require other modes
+(require 'dcpu16-mode)
+(require 'puppet-mode)
+
+;; Mode Mappings
+(require 'mode-mappings)
 
