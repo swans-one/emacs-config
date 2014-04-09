6646ca008cdec7e6e09804e69b771b731e7972e3
Erik Swanson <theerikswanson@gmail.com>
2014-04-09 16:20:50 -0400

    More ReStructuredText color changes.

--- init.el
+++ init.el
@@ -132,4 +132,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(rst-level-1 ((t (:background "black")))))
+ '(rst-level-1 ((t (:background "black"))))
+ '(rst-level-2 ((t (:background "color-17")))))
