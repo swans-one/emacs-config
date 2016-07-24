cc5b2ecf97a1c169c22e5820b6b73ecee0de6c63
Erik Swanson <theerikswanson@gmail.com>
2016-07-24 13:06:11 -0400

    Paren-matching mode hooks

--- site-lisp/hooks.el
+++ site-lisp/hooks.el
@@ -1,12 +1,36 @@
 ;; Mode specific hooks allow customization of behavior when a major
 ;; mode is loaded.
 
-(add-hook 'scheme-mode-hook 'show-paren-mode)
-(add-hook 'scheme-mode-hook 'paredit-mode)
+;; Global Hooks
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+
+;; Mode Hooks
+;;;;;;;;;;;;;
+
+;; emacs-lisp
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
 
+;; rust
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
+
+;; ruby
+(add-hook 'ruby-mode-hook 'ruby-electric-mode)
+(add-hook 'ruby-mode-hook (lambda () (auto-pair-mode -1)))
+
+;; scheme
+(add-hook 'scheme-mode-hook 'show-paren-mode)
+(add-hook 'scheme-mode-hook 'paredit-mode)
+
+;; term
 (add-hook 'term-mode-hook (lambda () (yas-minor-mode -1)))
+
+;; web
+(defun my/web-mode-hook ()
+  "Hooks for web mode"
+  (setq web-mode-markup-indent-offset 2))
+(add-hook 'web-mode-hook 'my/web-mode-hook)
+(add-hook 'web-mode-hook (lambda () (autopair-mode -1)))
 
 (provide 'hooks)
